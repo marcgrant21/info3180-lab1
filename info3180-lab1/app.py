569e9dcc55642522b6233ebcc7f0a1439f2a4cd5
testaa1 <43795666+marcgrant21@users.noreply.github.com>
2021-02-14 19:50:17 -0500

    created my First flask App

---
+++ info3180-lab1/app.py
@@ -0,0 +1,33 @@
+"""
+Flask Documentation:     http://flask.pocoo.org/docs/
+Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
+Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
+This file creates your application.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+'''
+# Routing for your application.
+# Put your routes below this comment
+'''
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """Custom 404 page."""
+    return render_template('404.html'), 404
+@app.route('/')
+
+def home():
+    return'My home page'
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port= 9090)
+    
